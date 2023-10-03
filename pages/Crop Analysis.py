eb5e8d13e2c556998679343ba3eb28071b670d2d
xyan <62557207+bryanlsx@users.noreply.github.com>
2023-10-03 20:52:13 +0800

    edit

--- pages/Crop Analysis.py
+++ pages/Crop Analysis.py
@@ -21,14 +21,14 @@ st.image("cluster_img_food_wl.png", caption="Cluster for Food Waste & Loss", use
 st.image("performance_measure.png", caption="Performance Measure of Attempted Models", use_column_width=True)
 
 st.header('Crop Analysis')
-selected_crop = st.selectbox('Choose a crop:', df_birch_wl_birch['Item'].unique())
+selected_crop = st.selectbox('Choose a crop (all crops are from the FAOSTAT dataset):', df_birch_wl_birch['Item'].unique())
 
 # Display clustering info based on selected crop
 prod_cluster = clusters_list_birch_foodprod[clusters_list_birch_foodprod['Item'] == selected_crop]['Cluster_Class'].values[0]
 prod_efficiency = {0: 'High production efficiency', 1: 'Low production efficiency', 2: 'Consistent Production Efficiency'}
-st.write(f"Production efficiency for ({selected_crop}) : {prod_efficiency[prod_cluster]}")
+st.write(f"Production efficiency for <{selected_crop}> : {prod_efficiency[prod_cluster]}")
 
 wl_cluster = clusters_list_birch_foodwl[clusters_list_birch_foodwl['Item'] == selected_crop]['Cluster_Class'].values[0]
 emission = {0: 'High GHG level Emission', 1: 'Moderate level GHG Emission', 2: 'Low level GHG Emission'}
-st.write(f"GHG emission level for ({selected_crop}) : {emission[wl_cluster]}")
+st.write(f"GHG emission level for <{selected_crop}> : {emission[wl_cluster]}")
 
