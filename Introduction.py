a6e975ff0486efcb62728bf17c98b11955f96546
xyan <62557207+bryanlsx@users.noreply.github.com>
2023-10-06 09:07:22 +0800

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -1,8 +1,45 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.figure_factory as ff
+import plotly.express as px
 
 st.title('Introduction')
 st.image("pg1.png", use_column_width=True)
 st.image("pg2.png", use_column_width=True)
+
+# ========Visualise Part===============
+st.header('GHG Emission Data Visualisations')
+
+data = pd.read_csv('Malaysia_GHG_Emission.csv',encoding='ISO-8859-1')
+
+# ==============================Total GHG Emissions by Source for latest year===========================================
+latest_year = ghg_data['Year'].max()
+emissions_by_source = ghg_data[ghg_data['Year'] == latest_year].groupby('Item')['Value'].sum().sort_values(ascending=False)
+
+# Create a horizontal bar chart using Plotly
+fig = px.bar(emissions_by_source, x=emissions_by_source.index, y=emissions_by_source.values, 
+             title=f'Total GHG Emissions by Source for {latest_year}',
+             labels={'y': 'Emission (kt)', 'x': 'Emission Source'},
+             color='Value')
+fig.update_layout(xaxis_tickangle=-45)
+# Displaying the plot on Streamlit
+st.plotly_chart(fig)
+
+st.divider()
+
+# ==============================Proportion of GHG Emissions by Element for latest year==============================
+emissions_by_element = ghg_data[ghg_data['Year'] == latest_year].groupby('Element')['Value'].sum().sort_values(ascending=False)
+
+# Creating an interactive pie chart using Plotly
+fig = px.pie(emissions_by_element, 
+             values=emissions_by_element.values, 
+             names=emissions_by_element.index,
+             title=f'Proportion of GHG Emissions by Element for {latest_year}')
+
+# Displaying the plot on Streamlit
+st.plotly_chart(fig)
+# =======================================
 st.image("pg3.png", use_column_width=True)
 st.image("pg4.png", use_column_width=True)
 st.image("pg5.png", use_column_width=True)
